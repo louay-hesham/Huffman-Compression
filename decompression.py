530b338c938954e61206445e391bc534c65151e6
Louay Morsi <louay.hesham@gmail.com>
2018-12-24 20:59:51 +0200

    Added decompression of folders

--- decompression.py
+++ decompression.py
@@ -1,4 +1,22 @@
 import time
+from os import mkdir
+
+def decode_header_folder_data(header):
+  header_str = header.decode()
+  folder_name = header_str.split('?')[0]
+  bits_lengths_str = header_str.split('?')[1]
+  bits_lengths = {}
+  for data_item in bits_lengths_str.split('|')[:-1]:
+    bits_lengths[data_item.split(':')[0]] = data_item.split(':')[1]
+  
+  header_data = header_str.split('?')[2].split('#')
+  huffman_codes = {}
+  for data_item in header_data:
+    data = data_item.split(':')
+    if (len(data) == 2):
+      huffman_codes[data[1]] = data[0]
+  
+  return huffman_codes, bits_lengths
 
 def decode_header_data(header):
   header_str = header.decode()
@@ -10,6 +28,16 @@ def decode_header_data(header):
     if (len(data) == 2):
       huffman_codes[data[1]] = data[0]
   return huffman_codes, bits_length
+
+def decode_folder(folder):
+  with open(folder, "rb") as file:
+    data = file.read()
+    file.close()
+  header_end = data.index(bytes('~'.encode()))
+  header = data[0:header_end]
+  actual_data = data[header_end + 1:]
+  codes, bits_lengths = decode_header_folder_data(header)
+  return codes, actual_data, bits_lengths
 
 def decode_file(filename):
   with open(filename, "rb") as file:
@@ -63,4 +91,23 @@ def decompress(filename):
   print("Time elapsed: " + str(t2 - t1) + " ms")
 
 def decompress_folder(foldername):
-  return 0;
+  t1 = time.time() * 1000
+  codes, total_data, bits_lengths = decode_folder(foldername)
+  total_binary_str = get_binary_string(codes, total_data)
+  old_i = 0
+  foldername = foldername.split('.')[0]
+  foldername += '_decompressed'
+  mkdir(foldername)
+  for filename, bits_length in bits_lengths.items():
+    bits_length = int(bits_length)
+    binary_str = total_binary_str[old_i: old_i + bits_length]
+    old_i += bits_length
+    decoded_data = decode_binary_str(codes, binary_str, bits_length)
+
+    filename += '.decompressed'
+
+    with open(foldername + '/' + filename, 'wb') as file:
+      file.write(decoded_data)
+      file.close()
+    t2 = time.time() * 1000
+    print("Time elapsed: " + str(t2 - t1) + " ms")
--- main.py
+++ main.py
@@ -2,33 +2,34 @@ from compression import compress, compress_folder
 from decompression import decompress, decompress_folder
 
 # For testing
-foldername = "test/"
-compress_folder(foldername)
+# foldername = "test/"
+# compress_folder(foldername)
+# decompress_folder("test.compressed")
 
 # Main code
 
-# while True:
-#   print("Choose an option:")
-#   print("1- Compress a file")
-#   print("2- Decompress a file")
-#   print("3- Compress a folder")
-#   print("4- Decompress a folder")
-#   print("0- Exit")
-#   choice = input("Choice > ")
-#   if choice == '1':
-#     filename = input("Enter filename: ")
-#     compress(filename)
-#   elif choice == '2':
-#     filename = input("Enter filename: ")
-#     decompress(filename)
-#   elif choice == '3':
-#     foldername = input("Enter folder name: ")
-#     compress_folder(foldername)
-#   elif choice == '4':
-#     foldername = input("Enter folder name: ")
-#     decompress_folder(foldername)
-#   elif choice == '0':
-#     break
-#   else:
-#     print("Invalid choice")
-#   print("\n\n")
+while True:
+  print("Choose an option:")
+  print("1- Compress a file")
+  print("2- Decompress a file")
+  print("3- Compress a folder")
+  print("4- Decompress a folder")
+  print("0- Exit")
+  choice = input("Choice > ")
+  if choice == '1':
+    filename = input("Enter filename: ")
+    compress(filename)
+  elif choice == '2':
+    filename = input("Enter filename: ")
+    decompress(filename)
+  elif choice == '3':
+    foldername = input("Enter folder name: ")
+    compress_folder(foldername + '/')
+  elif choice == '4':
+    foldername = input("Enter folder name: ")
+    decompress_folder(foldername)
+  elif choice == '0':
+    break
+  else:
+    print("Invalid choice")
+  print("\n\n")
